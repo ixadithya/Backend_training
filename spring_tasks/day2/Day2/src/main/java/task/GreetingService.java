ff990230f45a4b7aff1ce1e85e8ab3befaaa5262
ixadithya <adithya.reddy@innovatechs.com>
2026-03-09 05:26:00 +0530

    Add overloaded greet methods to GreetingServices

--- spring_tasks/day2/Day2/src/main/java/task/GreetingService.java
+++ spring_tasks/day2/Day2/src/main/java/task/GreetingService.java
@@ -8,4 +8,18 @@ public class GreetingService {
     public void greet() {
         System.out.println("Hello from GreetingService!");
     }
+
+    public void greet(String name) {
+        System.out.println("Welcome " + name);
+    }
+
+    public void greet(String name, String occasion) {
+        System.out.println("Happy " + occasion + " " + name + "!");
+    }
+
+    public void greet(String name, String occasion, String fromName) {
+        System.out.println("Happy " + occasion + " " + name + " from " + fromName + "!");
+    }
+
+    
 }

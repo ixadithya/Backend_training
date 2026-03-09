bb74b9387300a02937a1571660879e035f57d9fb
ixadithya <adithya.reddy@innovatechs.com>
2026-03-09 17:41:35 +0530

    Update A.java

--- spring_tasks/day1/task1/Task1/src/main/java/task3/A.java
+++ spring_tasks/day1/task1/Task1/src/main/java/task3/A.java
@@ -1,5 +1,6 @@
 package task3;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -9,11 +10,17 @@ public class A {
     B b;
 
     public A() {
-        System.out.println("A created");
+        System.out.println("A object created");
     }
 
     @Autowired
     public void setB(B b) {
+        System.out.println("Injecting B into A");
         this.b = b;
+    }
+
+    @PostConstruct
+    public void init() {
+        System.out.println("PostConstruct A");
     }
 }
